91428cde076def11c68c3bc05f1c42db59442e49
rikanga <richardkangamba3@gmail.com>
2023-03-11 07:13:34 +0100

    requirements.txt file added

--- videotester.py
+++ videotester.py
@@ -4,7 +4,9 @@ import numpy as np
 from keras.preprocessing import image
 import warnings
 warnings.filterwarnings("ignore")
-from keras.preprocessing.image import load_img, img_to_array 
+from tensorflow.keras.utils import load_img, img_to_array
+#from keras.preprocessing.image import img_to_array 
+#from keras.preprocessing.image import load_img, img_to_array 
 from keras.models import  load_model
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,7 +32,8 @@ while True:
         cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
         roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
         roi_gray = cv2.resize(roi_gray, (224, 224))
-        img_pixels = image.img_to_array(roi_gray)
+        #img_pixels = image.img_to_array(roi_gray)
+        img_pixels = img_to_array(roi_gray)
         img_pixels = np.expand_dims(img_pixels, axis=0)
         img_pixels /= 255
 
@@ -45,7 +48,7 @@ while True:
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ', resized_img)
+    cv2.imshow('Facial emotion analysis. CT THIERRY', resized_img)
 
     if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
         break
